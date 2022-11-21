594a7fefbc1419ccb79f4d1260492d634c55fa9a
shuufei <digital.shuufei@gmail.com>
2022-11-21 21:35:26 +0900

    fix: indexページとmeta情報の修正

--- apps/desktop-web-app/app/routes/index.tsx
+++ apps/desktop-web-app/app/routes/index.tsx
@@ -1,32 +1,8 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li className="text-red-500">
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main>
+      <h1>Puddle</h1>
+      <p>desktop web app</p>
+    </main>
   );
 }
