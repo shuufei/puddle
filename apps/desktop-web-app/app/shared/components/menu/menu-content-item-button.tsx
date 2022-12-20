88b4280cbd167c534a051957c1ce6e9d985a3a4f
shuufei <digital.shuufei@gmail.com>
2022-12-21 00:25:12 +0900

    fix: menu item buttonをclickableにする

--- apps/desktop-web-app/app/shared/components/menu/menu-content-item-button.tsx
+++ apps/desktop-web-app/app/shared/components/menu/menu-content-item-button.tsx
@@ -4,12 +4,14 @@ export const MenuContentItemButton: FC<{
   label: string;
   icon?: ReactNode;
   role?: 'normal' | 'danger';
-}> = ({ label, icon, role = 'normal' }) => {
+  onClick?: () => void;
+}> = ({ label, icon, role = 'normal', onClick }) => {
   return (
     <button
       className={`flex items-center text-xs pl-2 pr-4 py-1 rounded-sm hover:bg-gray-100 ${
         role === 'normal' ? 'text-gray-900' : 'text-red-500'
       } ${icon != null ? 'gap-2' : ''}`}
+      onClick={onClick}
     >
       {icon && icon}
       {label}
