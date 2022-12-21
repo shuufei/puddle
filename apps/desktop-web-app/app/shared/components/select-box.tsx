e3eae853940a5d41a04d54e089a8a1a46db63419
shuufei <digital.shuufei@gmail.com>
2022-12-22 00:20:53 +0900

    feat: select-box componentを追加

---
+++ apps/desktop-web-app/app/shared/components/select-box.tsx
@@ -0,0 +1,33 @@
+import type { FC } from 'react';
+import { useField } from 'react-aria';
+
+export type SelectOption = {
+  value: string;
+  label: string;
+};
+
+export const SelectBox: FC<{
+  label: string;
+  options: SelectOption[];
+}> = ({ label, options }) => {
+  const { labelProps, fieldProps } = useField({ label });
+  return (
+    <div className="flex flex-col gap-1">
+      <label className="text-sm text-gray-900" {...labelProps}>
+        {label}
+      </label>
+      <select
+        className="py-1 px-1.5 text-sm w-full border border-gray-400 rounded-sm"
+        {...fieldProps}
+      >
+        {options.map((option) => {
+          return (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
