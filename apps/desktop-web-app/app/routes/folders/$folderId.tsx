4f35a0110d10c312e7db9ba697b118ccaf6e0876
shuufei <digital.shuufei@gmail.com>
2022-12-23 22:31:11 +0900

    fix: sub folder表示を無効

--- apps/desktop-web-app/app/routes/folders/$folderId.tsx
+++ apps/desktop-web-app/app/routes/folders/$folderId.tsx
@@ -3,7 +3,7 @@ import { json } from '@remix-run/cloudflare';
 import { useCatch, useLoaderData } from '@remix-run/react';
 import type { CatchBoundaryComponent } from '@remix-run/react/dist/routeModules';
 import type { FC } from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 import type { Folder } from '~/domain/folder';
 import type { Item } from '~/domain/raindrop/item';
 import { NotFound } from '~/errors/not-found';
@@ -11,19 +11,19 @@ import { getRequestRaindropAccessToken } from '~/features/auth/get-request-raind
 import { getRequestUserId } from '~/features/auth/get-request-user-id.server';
 import { getFolderById } from '~/features/folder/api/get-folder-by-id.server';
 import { getFolderItems } from '~/features/folder/api/get-items.server';
-import { getSubFoldersByParentId } from '~/features/folder/api/get-subfolders-by-parent-id.server';
-import { CreateFolderButton } from '~/features/folder/components/create-folder-button';
+// import { getSubFoldersByParentId } from '~/features/folder/api/get-subfolders-by-parent-id.server';
+// import { CreateFolderButton } from '~/features/folder/components/create-folder-button';
 import { FolderConditions } from '~/features/folder/components/folder-conditions';
-import { FolderListNavigation } from '~/features/folder/components/folder-list-navigation';
+// import { FolderListNavigation } from '~/features/folder/components/folder-list-navigation';
 import { FolderMenu } from '~/features/folder/components/folder-menu';
 import { RaindropListItem } from '~/features/folder/components/raindrop-list-item';
-import { Tab } from '~/shared/components/tabs/tab';
+// import { Tab } from '~/shared/components/tabs/tab';
 import { handleLoaderError } from '~/shared/utils/handle-loader-error';
 
 type LoaderData = {
   folder: Folder;
   items: Item[];
-  subFolders: Folder[];
+  // subFolders: Folder[];
 };
 
 export const loader: LoaderFunction = async ({ request, params }) => {
@@ -34,20 +34,24 @@ export const loader: LoaderFunction = async ({ request, params }) => {
     if (folderId == null || Number.isNaN(folderId)) {
       throw new NotFound();
     }
-    const [folderRes, itemsRes, subFoldersRes] = await Promise.all([
+    const [
+      folderRes,
+      itemsRes,
+      // subFoldersRes
+    ] = await Promise.all([
       getFolderById(folderId, userId),
       getFolderItems(folderId, {
         userId,
         raindropAccessToken: accessToken,
       }),
-      getSubFoldersByParentId(folderId, userId),
+      // getSubFoldersByParentId(folderId, userId),
     ]);
     if (folderRes.data.data?.[0] == null) {
       throw new NotFound();
     }
     const response: LoaderData = {
       items: itemsRes.items,
-      subFolders: subFoldersRes.data.data as Folder[],
+      // subFolders: subFoldersRes.data.data as Folder[],
       folder: folderRes.data.data[0] as Folder,
     };
     return json(response);
@@ -57,8 +61,8 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 };
 
 const FolderPage: FC = () => {
-  const { folder, items, subFolders } = useLoaderData<LoaderData>();
-  const [activeTab, setActiveTab] = useState<'items' | 'subfodlers'>('items');
+  const { folder, items } = useLoaderData<LoaderData>();
+  // const [activeTab, setActiveTab] = useState<'items' | 'subfodlers'>('items');
   return (
     <div className="p-4">
       <div className="flex justify-between pr-4">
@@ -75,7 +79,16 @@ const FolderPage: FC = () => {
           onClickDeleteMenu={() => {}}
         />
       </div>
-      <div className="mt-6 flex gap-1 px-4">
+      <ul className="mt-6">
+        {items.map((item) => {
+          return (
+            <li key={item._id} className="mb-3">
+              <RaindropListItem raindropItem={item} />
+            </li>
+          );
+        })}
+      </ul>
+      {/* <div className="mt-6 flex gap-1 px-4">
         <Tab
           label="items"
           isActive={activeTab === 'items'}
@@ -115,7 +128,7 @@ const FolderPage: FC = () => {
             <CreateFolderButton />
           </div>
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
