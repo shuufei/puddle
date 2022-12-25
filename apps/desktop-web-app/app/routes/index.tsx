d1eb5251892154b99a72ebbce0099d15deefddf0
shuufei <digital.shuufei@gmail.com>
2022-12-25 19:29:11 +0900

    fix: rootへのアクセスを/foldersにredirect

--- apps/desktop-web-app/app/routes/index.tsx
+++ apps/desktop-web-app/app/routes/index.tsx
@@ -1,8 +1,7 @@
 import type { LoaderFunction } from '@remix-run/cloudflare';
 import { json } from '@remix-run/cloudflare';
-import { Link, useLoaderData } from '@remix-run/react';
-import { useCallback } from 'react';
-import { getSupabaseForBrowser } from '~/libs/supabase/supabase-client';
+import { useLoaderData, useNavigate } from '@remix-run/react';
+import { useEffect } from 'react';
 import { useSetCookieIfNeeded } from '~/shared/hooks/use-set-cookie-if-needed';
 
 type LoaderData = {
@@ -26,53 +25,16 @@ export const loader: LoaderFunction = async ({ request }) => {
 
 export default function Index() {
   const {
-    env: { supabaseUrl, supabaseAnonKey, endpoint },
+    env: { endpoint },
   } = useLoaderData<LoaderData>();
-  useSetCookieIfNeeded(endpoint);
+  const { authenticated } = useSetCookieIfNeeded(endpoint);
+  const navigate = useNavigate();
 
-  const signInWithGoogle = useCallback(async () => {
-    const supabase = getSupabaseForBrowser(supabaseUrl, supabaseAnonKey);
-    await supabase.auth.signInWithOAuth({
-      provider: 'google',
-      options: {
-        redirectTo: `${endpoint}`,
-      },
-    });
-  }, [endpoint, supabaseAnonKey, supabaseUrl]);
+  useEffect(() => {
+    if (authenticated) {
+      navigate('/folders');
+    }
+  }, [authenticated, navigate]);
 
-  const integrateRaindrop = useCallback(() => {
-    location.href = '/api/raindrop/authorize';
-    return;
-  }, []);
-
-  return (
-    <main>
-      <h1>Puddle</h1>
-      <p>desktop web app</p>
-      <section className="p-4">
-        <h2>Auth</h2>
-        <div className="flex gap-2">
-          <button
-            className="bg-green-600 px-3 py-2 rounded-sm text-white"
-            onClick={signInWithGoogle}
-          >
-            sign in with google
-          </button>
-          <button
-            className="bg-blue-600 px-3 py-2 rounded-sm text-white"
-            onClick={integrateRaindrop}
-          >
-            integrate Raindrop
-          </button>
-        </div>
-      </section>
-      <nav>
-        <ul>
-          <li>
-            <Link to={'/folders'}>/folders</Link>
-          </li>
-        </ul>
-      </nav>
-    </main>
-  );
+  return null;
 }
--- apps/desktop-web-app/app/shared/hooks/use-set-cookie-if-needed.ts
+++ apps/desktop-web-app/app/shared/hooks/use-set-cookie-if-needed.ts
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useSetCookieIfNeeded = (endpoint: string) => {
+  const [authenticated, setAuthenticated] = useState(false);
   useEffect(() => {
     const hash = window.location.hash;
     console.log('[DEBUG] url hash: ', hash);
@@ -10,10 +11,14 @@ export const useSetCookieIfNeeded = (endpoint: string) => {
         method: 'POST',
         body: JSON.stringify({ hash }),
       });
+      setAuthenticated(true);
     };
     if (shouldSetCookie) {
       setCookie();
       return;
     }
+    setAuthenticated(true);
   }, [endpoint]);
+
+  return { authenticated };
 };
